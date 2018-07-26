f5a491b440d9b26ae3be5d4daa2c21e7fd28e3ef
Derrick Thai <derrickthai3690@gmail.com>
2018-07-26 02:36:08 -0700

    in progress

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ var mainState = {
         //name image on the left, give path to image on the right 
         game.load.image('player', 'media/doggo.png');
         game.load.image('wall', 'media/wall.png');
-        game.load.image('coin', 'media/coke.png');
-        game.load.image('enemy', 'media/enemy.png');
+        game.load.image('coke', 'media/coke.png');
+        game.load.image('enemy', 'media/depression.png');
         game.load.image('chef', 'media/chef.png');
-        game.load.audio('audio', 'media/bird.m4a');
-        game.load.tilemap('map', 'media/Doggo.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('tileset', 'media/strawberry_cake_tileset_small.png');
         game.load.image('egg','media/egg.png');
-        game.load.audio('audio','media/bird.m4a');
+        game.load.image('background', 'media/openconceptkitchen.png')
+        game.load.audio('audio', 'media/bird.m4a');
 
     },
 
@@ -20,7 +18,7 @@ var mainState = {
         // Here we create the game
 
         //change the game's background color
-        game.stage.backgroundColor = '#3598db';
+        background = game.add.tileSprite(0, 0, 1400, 700, 'background');
 
         // Start the Arcade physics system (for movements and collisions)
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -33,32 +31,33 @@ var mainState = {
         // Create the player in the middle of the game
         this.player = game.add.sprite(70, 100, 'player');
         this.score = 0;
+        
         // Add gravity to make it fall
-        this.player.body.gravity.y = 600;
+        this.player.body.gravity.y = 1000;
 
         // Create 3 groups that will contain our objects
         this.walls = game.add.group();
-        this.coins = game.add.group();
+        this.cokes = game.add.group();
         this.enemies = game.add.group();
         this.chefs = game.add.group();
         
-        this.danger = game.add.sprite(200, 95, 'chef')
+        this.danger = game.add.sprite(200, 200, 'chef')
         this.enemies.add(this.danger);
-        this.tween = game.add.tween(this.danger).to( { x:400, y:95 }, 1000, "Linear", true, 0, -1);
+        this.tween = game.add.tween(this.danger).to( { x:400, y:200 }, 1000, "Linear", true, 0, -1);
         
         this.tween.yoyo(true, 0);
         
-        // Design the level. x = wall, o = coin, ! = lava.
+        // Design the level. x = wall, o = coke, ! = depression.
         var level = [
             'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             'x                                                                                x',
             'x                                                                                x',
-            'x                                                                                x',
-            'x                                                                                x',
-            'x                                                                                x',
-            'x                                                                                x', 
-            'x                                                                                x',
-            'x                                                                                x',
+            'x                                    o                                           x',
+            'x                    o               x                                           x',
+            'x                   xxx            !   !             c                            x',
+            'xxxxxxxxxxxxx     xx   xx    xxxx!!!!!!!!!xxxx    x !x !x !x !x !x !x            x', 
+            'x             !!!                                                                x',
+            'x                         !!                                                     x',
             'x                                                                                x',
             'x                                                                                x',
             'x                                                                                x',
@@ -93,8 +92,11 @@ var mainState = {
                     var enemy = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'enemy');
                     this.enemies.add(enemy);
                 } else if (level[i][j] === 'o') {
-                    var coin = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'coin');
-                    this.coins.add(coin);
+                    var coke = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'coke');
+                    this.cokes.add(coke);
+                } else if (level[i][j] === 'c') {
+                    var chef = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'chef');
+                    this.chefs.add(chef);
                 }
 
 
@@ -111,7 +113,7 @@ var mainState = {
         game.physics.arcade.collide(this.player, this.walls);
 
         //check for player and coins overlapping
-        game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
+        game.physics.arcade.overlap(this.player, this.cokes, this.takeCoke, null, this);
 
         //check for player and enemy overlapping
         game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
@@ -132,16 +134,16 @@ var mainState = {
             this.player.body.velocity.x = 0;
         }
         if (this.cursor.up.isDown && this.player.body.touching.down) {
-            this.player.body.velocity.y = -200;
+            this.player.body.velocity.y = -300;
         }
         if(this.cursor.down.isDown){
             game.state.restart()
         }
     },
 
-    takeCoin: function (player, coin) {
+    takeCoke: function (player, coke) {
         this.score++;
-        coin.kill();
+        coke.kill();
          game.sound.play('audio')
     },
     restart: function () {
